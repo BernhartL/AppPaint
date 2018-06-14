887de9624c2c2e1606f33eebb54b89ba2671c25c
BernhartL <38957685+BernhartL@users.noreply.github.com>
2018-06-14 21:38:19 +0200

    1.0.0

--- www/application.js
+++ www/application.js
@@ -1,11 +1,18 @@
 document.addEventListener('deviceready', function() {
-    // erst jetzt ist APP geladen und ready um was zu tun!
-    console.log( 'DEVICE READY!' );
-    $( document ).ready( function() {
-      console.log( 'DOM READY!' );
-    });
+  // erst jetzt ist APP geladen und ready um was zu tun!
+  console.log( 'DEVICE READY!' );
+
+  $( document ).ready( function() {
+    console.log( 'DOM READY!' );
+
+    var c = $('<canvas>', { width:$(window).width(), height:$(window).height() })
+      .appendTo('body');
+
+      var ctx = c.get(0).getContext('2d');
+      ctx.fillRext(100,100,100,100);
 
 
+  });
 
 
 });
